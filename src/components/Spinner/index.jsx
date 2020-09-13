ca258e9fc9a10668715ced5418878774a5b70c91
Vootele Verrev <vootele.verrev@proekspert.ee>
2020-09-13 19:33:14 +0300

    Spinner

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,3 +1,13 @@
 import React from "react";
+import styles from "components/Spinner/styles.scss";
 
-export default () => <div>SPINNER</div>;
+export default () => (
+  <div className={styles.spinner}>
+    <div className={styles.skFoldingCube}>
+      <div className={`${styles.skCube1} ${styles.skCube}`} />
+      <div className={`${styles.skCube2} ${styles.skCube}`} />
+      <div className={`${styles.skCube3} ${styles.skCube}`} />
+      <div className={`${styles.skCube4} ${styles.skCube}`} />
+    </div>
+  </div>
+);
